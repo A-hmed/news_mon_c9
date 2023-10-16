c35c9a6abac8d6eff6b0f5bf3b79916dc5561b65
Ahmed <ahmed.nabil1@nomadz.dev>
2023-10-16 20:06:19 +0300

    Added Side menu.

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:news_mon_c9/model/category_dm.dart';
 import 'package:news_mon_c9/ui/screens/home/tabs/categories/categories_tab.dart';
 import 'package:news_mon_c9/ui/screens/home/tabs/news/news_tab.dart';
+import 'package:news_mon_c9/ui/screens/home/tabs/settings/settings.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "home";
@@ -11,31 +12,75 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  CategoryDM? currentCategory;
+  late Widget currentWidget ;
+  late Widget categoriesTab;
+  @override
+  void initState() {
+    super.initState();
+    categoriesTab = CategoriesTab(onCategoryClicked);
+    currentWidget = categoriesTab;
+  }
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: (){
-        if(currentCategory == null){
-          return Future.value(true);
-        }else {
-          currentCategory = null;
+      onWillPop: () async{
+        if(currentWidget != categoriesTab){
+          currentWidget = CategoriesTab(onCategoryClicked);
           setState(() {});
-          return Future.value(false);
+          return false;
+        }else {
+         return true;
         }
       },
       child: Scaffold(
         appBar: AppBar(
           title: const Text("News app!"),
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(22))),
         ),
-        body: currentCategory == null ? CategoriesTab(onCategoryClicked) :
-        NewsTab(categoryId: currentCategory!.id),
+        body: currentWidget,
+        drawer:  Drawer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+               DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Center(child: Text("News app")),
+              ),
+              buildDrawerRow(Icons.list, "Categories", (){
+                currentWidget = categoriesTab;
+                Navigator.pop(context);
+                setState(() {});
+              }),
+              buildDrawerRow(Icons.settings, "Settings", (){
+                currentWidget = SettingsTab();
+                Navigator.pop(context);
+                setState(() {});
+              })
+            ],
+          ),
+        ),
       ),
     );
   }
 
   onCategoryClicked(CategoryDM clickedCategory){
-    currentCategory = clickedCategory;
+    currentWidget = NewsTab(categoryId: clickedCategory.id);
     setState(() {});
+  }
+
+  buildDrawerRow(IconData iconData, String title, Function onClick){
+    return InkWell(
+      onTap: (){
+        onClick();
+      },
+      child: Row(
+        children: [
+          SizedBox(width: 8,),
+          Icon(iconData),
+          SizedBox(width: 4,),
+          Text(title),
+        ],
+      ),
+    );
   }
 }
